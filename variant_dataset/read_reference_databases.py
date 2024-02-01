f1d9dfb48d4deb61768465e17ce86040b36d74a5
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2024-02-01 15:28:26 -0500

    update read reference datasets

--- variant_dataset/read_reference_databases.py
+++ variant_dataset/read_reference_databases.py
@@ -10,10 +10,10 @@ import os
 import joblib
 from variant_dataset.data_utils import Protein, Variant
 from Bio.PDB.Polypeptide import protein_letters_3to1
-logging.basicConfig(filename='/home/dzeiberg/variant_dataset/log.txt', encoding='utf-8', level=logging.DEBUG)
-server = "https://rest.ensembl.org"
-ext = "/archive/id"
-headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
+import urllib
+
+logging.basicConfig(filename='/tmp/log.txt', encoding='utf-8', level=logging.DEBUG)
+
 
 
 def read_gzip_fasta(path):
@@ -23,70 +23,29 @@ def read_gzip_fasta(path):
             records[record.id] = str(record.seq)
     return records
 
-def read_gnomad(path="/data/dzeiberg/gene_calibration/gnomad.genomes_and_exomes.r2.1.1.sites.pkl"):
-    gnomad = pd.read_pickle(path)
-    return gnomad
-
-def read_gnomad_and_ensembl(path="/data/dzeiberg/gene_calibration/gnomad.genomes_and_exomes.r2.1.1.sites.pkl",
-                ensemble_path="/data/dbs/ensembl/Homo_sapiens.GRCh38.pep.all.fa.gz",
-                cached_ensembl_file="/home/dzeiberg/variant_dataset/ensembl_records.pkl"):
-    gnomad = read_gnomad(path)
-   
-    ensembl_records = read_gzip_fasta(ensemble_path)
-    gnomad = gnomad[gnomad.HGVSp != "."]
-    return gnomad, ensembl_records
-
-def load_preprocessed_gnomad(gnomad_path="/data/dzeiberg/gene_calibration/vikas_calibration_gnomad_dataset.pkl",
-                                ensemble_path="/data/dbs/ensembl/Homo_sapiens.GRCh38.pep.all.fa.gz"):
-    gnomad = pd.read_pickle(gnomad_path)
-    ensembl = read_gzip_fasta(ensemble_path)
-    grouped = gnomad.groupby("Ensembl_proteinid_latest")
-    proteins = {}
-    for ensembl_transcript_id,variants in tqdm(grouped,total=len(grouped)):
-        try:
-            wt_seq = ensembl[ensembl_transcript_id]
-        except KeyError:
-            continue
-        protein = Protein(ensembl_transcript_id, wt_seq)
-        for _,row in variants.iterrows():
-            aa_ref = row.aavar[0]
-            aa_pos = int(row.aavar[1:-1])
-            aa_alt = row.aavar[-1]
-            try:
-                variant = Variant(protein,aa_ref, aa_pos, aa_alt)
-            except ValueError as e:
-                print(e)
-                continue
-            for k,v in row.items():
-                variant.add_annotation(k,v)
-            protein.add_variant(variant)
-        proteins[protein.sequence_hash] = protein
-    return proteins
-
-
-def process_gnomad(gnomad_df : DataFrame, ensembl_records : dict):
+def process_gnomad(df, ensembl_records):
     gnomad_proteins = dict()
-    gnomad_df = gnomad_df.assign(ensembl_transcript_id=gnomad_df.HGVSp.str.split(":").str[0])
-    grouped = gnomad_df.groupby("ensembl_transcript_id")
-    for ensembl_transcript_id,variants in tqdm(grouped,total=len(grouped)):
+    for Ensembl_protein_id, variants in tqdm(df.groupby("Ensembl_protein_id"),total=len(df.Ensembl_protein_id.unique())):
         try:
-            wt_seq = ensembl_records[ensembl_transcript_id]
+            wt_seq = ensembl_records[Ensembl_protein_id]
         except KeyError:
+            logging.warning(f"{Ensembl_protein_id} not present in ensembl records")
             continue
-        protein = Protein(ensembl_transcript_id, wt_seq)
+        protein = Protein(Ensembl_protein_id, wt_seq)
         for _, row in variants.iterrows():
+            # row["HGCSp"] looks like ":p.Ala123Val"
             try:
                 substitution = row["HGVSp"].split(":")[1][2:]
                 try:
                     aa_ref = protein_letters_3to1[substitution[:3].upper()]
                     aa_alt = protein_letters_3to1[substitution[-3:].upper()]
                 except KeyError as e:
-                    # logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
+                    logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
                     continue
                 aa_pos = int(substitution[3:-3])
                 variant = Variant(protein,aa_ref, aa_pos, aa_alt)
             except ValueError as e:
-                # logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
+                logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
                 continue
             for annotation_name,annotation_value in row.items():
                 variant.add_annotation("gnomad_v211_" + annotation_name, annotation_value)
@@ -94,32 +53,101 @@ def process_gnomad(gnomad_df : DataFrame, ensembl_records : dict):
         gnomad_proteins[protein.sequence_hash] = protein
     return gnomad_proteins
 
-def process_clinvar(clinvar_pickle_path="/data/dbs/clinvar/clinvar_12_13_2023/variant_summary_annotated.pkl",
-                    mane_fasta_path="/data/dbs/mane/MANE.GRCh38.v1.1.refseq_protein.faa.gz",
-                    mane_summary_path="/data/dbs/mane/MANE.GRCh38.v1.1.summary.txt"):
-    clinvar_df = pd.read_pickle(clinvar_pickle_path)
-    mane_records = read_gzip_fasta(mane_fasta_path)
-    mane_summary = pd.read_csv(mane_summary_path,sep="\t")
-    mane_summary = mane_summary[mane_summary.MANE_status == "MANE Select"].set_index("symbol")
+
+def read_gnomad_vcf_files(*args,**kwargs):
+    gnomad_exomes = kwargs.get("gnomad_exomes")
+    gnomad_genomes = kwargs.get("gnomad_genomes")
+    header_file = kwargs.get("gnomad_header_file")
+
+    with open(header_file) as f:
+        columns = f.read().strip().split(",")
+    
+    exomes = pd.read_csv(gnomad_exomes,delimiter='\t',header=None,compression='gzip')
+    exomes.columns = columns
+    genomes = pd.read_csv(gnomad_genomes,delimiter='\t',header=None,compression='gzip')
+    genomes.columns = columns
+
+    gnomad = pd.concat((exomes,genomes))#.drop_duplicates(subset=['HGVSp'])
+    gnomad = gnomad.assign(Ensembl_protein_id = gnomad.HGVSp.str.split(":").str[0])
+    gnomad.AF = pd.to_numeric(gnomad.AF,errors='coerce')
+    return gnomad
+
+def process_clinvar(clinvar_variant_summary,mane_summary,mane_records, disease_genes):
+    mane_summary = mane_summary.reset_index().set_index("GeneID")
+    clinvar_snvs = clinvar_variant_summary[clinvar_variant_summary.Type == "single nucleotide variant"]
+    clinvar_snvs = clinvar_snvs.assign(GeneID = clinvar_snvs.GeneID.astype(str))
+    clinvar_snvs = clinvar_snvs[clinvar_snvs.GeneID.isin(disease_genes.GeneID)]
+    clinvar_snvs = clinvar_snvs[clinvar_snvs.Name.str.contains("p.")]
+    clinvar_snvs = clinvar_snvs.assign(HGVSp=":p."+clinvar_snvs.Name.str.split("p.").str[1].str.slice(0,-1))
     clinvar_proteins = dict()
-    grouped = clinvar_df.groupby("GeneSymbol")
-    for gene_symbol, variants in tqdm(grouped, total=len(grouped)):
+    for GeneID, variants in tqdm(clinvar_snvs.groupby("GeneID"),total=len(clinvar_snvs.GeneID.unique())):
         try:
-            canonical_RefSeq_prot = mane_summary.loc[gene_symbol.upper()].RefSeq_prot
-            protein = Protein(gene_symbol, mane_records[canonical_RefSeq_prot])
+            RefSeq_protein_id = mane_summary.loc[GeneID, "RefSeq_prot"]
         except KeyError as e:
-            logging.warning(f"Skipping protein {gene_symbol}: {e}")
+            logging.warning(f"Skipping protein {GeneID}: {e}")
             continue
+        try:
+            wt_seq = mane_records[RefSeq_protein_id]
+        except KeyError as e:
+            logging.warning(f"Skipping protein {GeneID}: {e}")
+            continue
+        protein = Protein(RefSeq_protein_id, wt_seq)
         for _, row in variants.iterrows():
             try:
-                variant = Variant(protein,protein_letters_3to1[row["wt_aa"].upper()],
-                                int(row['position']),
-                                protein_letters_3to1[row["mt_aa"].upper()])
+                substitution = row["HGVSp"].split(":")[1][2:]
+                try:
+                    aa_ref = protein_letters_3to1[substitution[:3].upper()]
+                    aa_alt = protein_letters_3to1[substitution[-3:].upper()]
+                except KeyError as e:
+                    logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
+                    continue
+                aa_pos = int(substitution[3:-3])
+                variant = Variant(protein,aa_ref, aa_pos, aa_alt)
             except ValueError as e:
-                logging.warning(f"Skipping variant {row['#AlleleID']} for protein {protein.id}\n{protein}\n: {e}")
+                logging.warning(f"Skipping variant {row['HGVSp']} for protein {protein.id}\n{protein}\n: {e}")
                 continue
             for annotation_name,annotation_value in row.items():
-                variant.add_annotation("clinvar20231213_" + annotation_name, annotation_value)
+                variant.add_annotation("clinvar" + annotation_name, annotation_value)
             protein.add_variant(variant)
         clinvar_proteins[protein.sequence_hash] = protein
     return clinvar_proteins
+
+def get_disease_genes(clinvar_variant_summary, mane_summary):
+    
+    disease_variants = clinvar_variant_summary[(clinvar_variant_summary.ClinicalSignificance.isin(["Pathogenic","Likely pathogenic", "Pathogenic/Likely pathogenic"])) & \
+                                               (~clinvar_variant_summary.ReviewStatus.isin({'no clasification provided', 'no assertion criteria provided','no classification for the single variant'}))]
+    disease_variants = disease_variants.assign(GeneID = disease_variants.GeneID.astype(str))
+    disease_genes = mane_summary[mane_summary.GeneID.isin(disease_variants.GeneID)]
+    return disease_genes
+    
+
+def read_steps(*args, **kwargs):
+    """
+    Required kwargs:
+    ensembl_fasta : str : e.g. path to 'Homo_sapiens.GRCh38.pep.all.fa.gz'
+    gnomad_exomes : str : e.g. path to gnomad.exomes.r2.1.1.sites.missense.vcf.gz : expect gzip tsv filepath
+    gnomad_genomes : str : e.g. path to gnomad.genomes.r2.1.1.sites.missense.vcf.gz : expect gzip tsv filepath
+    gnomad_header_file : str : path to column names in the gnomad files
+    """
+    clinvar_variant_summary = pd.read_csv("https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz",delimiter="\t",compression='gzip')
+
+    mane_summary = pd.read_csv("https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.2/MANE.GRCh38.v1.2.summary.txt.gz",compression='gzip',delimiter='\t')
+    mane_summary = mane_summary[mane_summary.MANE_status == "MANE Select"]
+    mane_summary = mane_summary.assign(GeneID=mane_summary.loc[:,"#NCBI_GeneID"].str.split(":").str[1])
+
+    urllib.request.urlretrieve("https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.2/MANE.GRCh38.v1.2.refseq_protein.faa.gz",'/tmp/MANE.GRCh38.v1.2.refseq_protein.faa.gz')
+    mane_records = read_gzip_fasta('/tmp/MANE.GRCh38.v1.2.refseq_protein.faa.gz')
+    # ~1 min , returns 4251 genes
+    # columns '#NCBI_GeneID, Ensembl_Gene, HGNC_ID, symbol, name, RefSeq_nuc, RefSeq_prot, Ensembl_nuc, Ensembl_prot, MANE_status, GRCh38_chr, chr_start, chr_end, chr_strand, GeneID'
+    disease_genes = get_disease_genes(clinvar_variant_summary,mane_summary)
+    gnomad_genomes_and_exomes = read_gnomad_vcf_files(**kwargs)
+    # 912304 unique variants (from HGVSp)
+    gnomad_sample = gnomad_genomes_and_exomes[(gnomad_genomes_and_exomes.FILTER == 'PASS') & \
+                                              (gnomad_genomes_and_exomes.AF < 0.01) & \
+                                                (gnomad_genomes_and_exomes.Ensembl_protein_id.isin(disease_genes.Ensembl_prot))]
+    ensembl = read_gzip_fasta(kwargs.get("ensembl_fasta"))
+    # 849254 variants, 2747 genes
+    gnomad_proteins = process_gnomad(gnomad_sample, ensembl)
+
+    clinvar_proteins = process_clinvar(clinvar_variant_summary,mane_summary,mane_records,disease_genes)
+    return clinvar_proteins, gnomad_proteins
